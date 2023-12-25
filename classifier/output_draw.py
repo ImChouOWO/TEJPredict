6caa9cee4b9966612fefa0264c6714dba8621345
ImChouOWO <as0930859852@gmail.com>
2023-12-25 10:33:39 +0800

    2023-12-25

--- classifier/output_draw.py
+++ classifier/output_draw.py
@@ -1,57 +1,75 @@
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
+from matplotlib.font_manager import FontProperties
+
+font_path = '/System/Library/Fonts/PingFang.ttc'
+chinese_font = FontProperties(fname=font_path)
 
 
-top_list = []
-bottom_list = []
 
-top_uniqe = []
-bottom_uniqe = []
+def create_id_to_name_mapping(nameID_df):
+    """
+    Create a mapping from company IDs to company names.
 
-def load_data(year:str,file_path:str):
-    global top_list
-    global bottom_list
+    :param nameID_df: DataFrame containing company IDs and names
+    :return: Dictionary mapping from company IDs to names
+    """
+    return dict(zip(nameID_df['ID'], nameID_df['公司名']))
 
+def load_data_and_replace_ids(year, file_path, id_to_name_map):
+    """
+    Load data from the given file path and replace company IDs with names using the provided mapping.
+
+    :param year: Year of the data
+    :param file_path: Path to the data file
+    :param id_to_name_map: Dictionary mapping from company IDs to names
+    :return: List of company names
+    """
+    data_list = []
     data = pd.read_excel(file_path)
-    try:
-        for i in list(data['top 20%'].values):
-            top_list.append(i)
 
-    except:
-        for i in list(data['bottom 20%']):
-            bottom_list.append(i)
-       
+    try:
+        column_name = 'top 20%' if 'top 20%' in data.columns else 'bottom 20%'
+        data_list = [id_to_name_map.get(i, i) for i in list(data[column_name].values)]
+    except KeyError as e:
+        print(f"Error processing file {file_path}: {e}")
+    
+    return data_list
 
 def draw_pie(data_list, title):
-    # 計算列表中各元素的出現次數
     count = Counter(data_list)
+    top_items = count.most_common(10)
 
-    # 取出現次數最多的前五個元素
-    top_five = count.most_common(10)
+    labels = [item[0] for item in top_items]
+    sizes = [item[1] for item in top_items]
 
-    # 準備餅圖數據
-    labels = [item[0] for item in top_five]
-    sizes = [item[1] for item in top_five]
-
-    # 繪製餅圖
     plt.figure(figsize=(8, 8))
     plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
-    plt.title(title)
-
-    # 顯示圖形
+    plt.title(title, fontproperties=chinese_font)
     plt.savefig(f"classifier/outputData/output/img/{title}.png")
-      
+    plt.show()
 
+def main():
+    nameID_df = pd.read_excel("dataset/cleanData/nameID.xlsx")
+    id_to_name_map = create_id_to_name_mapping(nameID_df)
 
-if __name__ =="__main__":
-   
-    for i in ["2013","2014","2015","2016","2017","2018","2019","2020","2021","2022"]:
-        file_top_path = f"classifier/outputData/output/predictData/final_output_top_{i}.xlsx"
-        file_bottom_path = f"classifier/outputData/output/predictData/final_output_bottom_{i}.xlsx"
+    top_list = []
+    bottom_list = []
 
-        load_data(year=i,file_path=file_top_path)
-        load_data(year=i,file_path=file_bottom_path)
-    draw_pie(top_list, 'Top 10 company')
-    draw_pie(bottom_list,"Bottom 10 company")
+    for year in ["2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"]:
+        file_top_path = f"classifier/outputData/output/predictData/final_output_top_{year}.xlsx"
+        file_bottom_path = f"classifier/outputData/output/predictData/final_output_bottom_{year}.xlsx"
+
+        top_list.extend(load_data_and_replace_ids(year, file_top_path, id_to_name_map))
+        bottom_list.extend(load_data_and_replace_ids(year, file_bottom_path, id_to_name_map))
+
+    draw_pie(top_list, 'Top 10 Companies')
+    draw_pie(bottom_list, "Bottom 10 Companies")
+
+if __name__ == "__main__":
+    main()
+
+    # draw_pie(top_list, 'Top 10 company')
+    # draw_pie(bottom_list,"Bottom 10 company")
     
